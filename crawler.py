bb2b60317647c635ce041e1b1a5442ef26d589fe
Christoph Friedrich <christoph@friedrich-whv.de>
2025-01-30 19:30:00 +0100

    Enable crawling based on multiple stops

--- crawler.py
+++ crawler.py
@@ -12,26 +12,29 @@ con = mysql.connector.connect(user=config['client']['user'], password=config['cl
 
 import urllib.request
 import json
-url = "https://whitelabel.bahnland-bayern.de/efa/XML_DM_REQUEST?commonMacro=dm&type_dm=any&name_dm=de%3A09663%3A22&outputFormat=rapidJSON&mode=direct&useRealtime=1&includeCompleteStopSeq=1&depType=stopEvents"
-with urllib.request.urlopen(url) as response:
-    data = json.load(response)
-    #for se in data['stopEvents']:
-    #    print(se['transportation']['id'], se['transportation']['properties']['tripCode'], se['departureTimePlanned'][0:10].replace('-',''))
-
-cursor = con.cursor()
-sql = "INSERT INTO trips (line, tripCode, date, departureTimePlanned, arrivalTimePlanned) VALUES (%s, %s, %s, CONVERT_TZ(%s, 'UTC', 'Europe/Berlin'), CONVERT_TZ(%s, 'UTC', 'Europe/Berlin')) ON DUPLICATE KEY UPDATE updated=NOW()"
-val = [(
-        stopEvent['transportation']['id'],
-        stopEvent['transportation']['properties']['tripCode'],
-        stopEvent['departureTimePlanned'][0:10].replace('-',''),
-        stopEvent['previousLocations'][0]['departureTimePlanned'],
-        stopEvent['onwardLocations'][-1]['arrivalTimePlanned']
-       )
-       for stopEvent in data['stopEvents']
-      ]
-print(val)
-cursor.executemany(sql, val)
-con.commit()
+crawlTasks = ['de:09663:22', 'de:09663:358']   # Adalberokirche, Skyline Hill Center
+for stopParentId in crawlTasks:
+    stopParentIdForUrl = stopParentId.replace(':', '%3A')
+    url = f"https://whitelabel.bahnland-bayern.de/efa/XML_DM_REQUEST?commonMacro=dm&type_dm=any&name_dm={stopParentIdForUrl}&outputFormat=rapidJSON&mode=direct&useRealtime=1&includeCompleteStopSeq=1&depType=stopEvents"
+    print(url)
+    with urllib.request.urlopen(url) as response:
+        data = json.load(response)
+        #for se in data['stopEvents']:
+        #    print(se['transportation']['id'], se['transportation']['properties']['tripCode'], se['departureTimePlanned'][0:10].replace('-',''))
+        cursor = con.cursor()
+        sql = "INSERT INTO trips (line, tripCode, date, departureTimePlanned, arrivalTimePlanned) VALUES (%s, %s, %s, CONVERT_TZ(%s, 'UTC', 'Europe/Berlin'), CONVERT_TZ(%s, 'UTC', 'Europe/Berlin')) ON DUPLICATE KEY UPDATE updated=NOW()"
+        val = [(
+                stopEvent['transportation']['id'],
+                stopEvent['transportation']['properties']['tripCode'],
+                stopEvent['departureTimePlanned'][0:10].replace('-',''),
+                stopEvent['previousLocations'][0]['departureTimePlanned'],
+                stopEvent['onwardLocations'][-1]['arrivalTimePlanned']
+            )
+            for stopEvent in data['stopEvents']
+            ]
+        print(val)
+        cursor.executemany(sql, val)
+        con.commit()
 
 cursor.execute("SELECT `id`, `line`, `tripCode`, `date` FROM `trips` WHERE `departureTimePlanned` < NOW() and `arrivalTimePlanned` < addtime(NOW(), '00:05:00') and NOT `completed`")
 rows = cursor.fetchall()
