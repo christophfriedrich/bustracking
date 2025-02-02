5024a02052f65530ea1bc16e713a8b5d97ab391c
Christoph Friedrich <christoph@friedrich-whv.de>
2025-02-02 22:46:12 +0100

    Fix returning and displaying trips without stopEvents
    Without the `v-if`, the site didn't render because of accessing `arrivalDelays` on a `null` object. Also, to get ALL trips (not just the ones that we caught data for), we need to `GROUP BY` a `trips` field (not a `stopEvents` field) and do a join that always fully includes that table. (Also unrelatedly fixing a path and a tab.)

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import configparser
 config = configparser.ConfigParser()
 config.read('/home/chrfrd/.my.cnf')
 
-app = Flask(__name__, static_folder='/home/chrfrd')
+app = Flask(__name__, static_folder='/home/chrfrd/srv/bustracking')
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = config['client']['user']
 app.config['MYSQL_PASSWORD'] = config['client']['password']
@@ -19,7 +19,7 @@ def frontend():
 @app.route('/api/delays', methods=['GET'])
 def get_data():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT line, date, tripCode, time_format(trips.departureTimePlanned, '%H:%i') AS start, time_format(trips.arrivalTimePlanned, '%H:%i') AS end, GROUP_CONCAT(time_to_sec(timediff(arrivalTimeEstimated, stopEvents.arrivalTimePlanned)) ORDER BY stopEvents.arrivalTimePlanned ASC) AS arrivalDelays, GROUP_CONCAT(time_to_sec(timediff(departureTimeEstimated, stopEvents.departureTimePlanned)) ORDER BY stopEvents.departureTimePlanned ASC) AS departureDelays FROM stopEvents LEFT JOIN stops ON stops.globalId=stopId LEFT JOIN trips ON stopEvents.tripId=trips.id WHERE line='wvv:10010:E:H:24d' AND date=20250129 GROUP BY tripId ORDER BY line, date, start''')
+    cur.execute('''SELECT line, date, tripCode, time_format(trips.departureTimePlanned, '%H:%i') AS start, time_format(trips.arrivalTimePlanned, '%H:%i') AS end, GROUP_CONCAT(time_to_sec(timediff(arrivalTimeEstimated, stopEvents.arrivalTimePlanned)) ORDER BY stopEvents.arrivalTimePlanned ASC) AS arrivalDelays, GROUP_CONCAT(time_to_sec(timediff(departureTimeEstimated, stopEvents.departureTimePlanned)) ORDER BY stopEvents.departureTimePlanned ASC) AS departureDelays FROM stopEvents LEFT JOIN stops ON stops.globalId=stopId RIGHT OUTER JOIN trips ON stopEvents.tripId=trips.id WHERE line='wvv:10010:E:H:24d' AND date=20250129 GROUP BY trips.id ORDER BY line, date, start''')
     data = cur.fetchall()
     cur.close()
     column_names = ['line', 'date', 'tripCode', 'start', 'end', 'arrivalDelays', 'departureDelays']
