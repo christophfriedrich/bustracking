415ac2b369192d2b71ea81b07caa906a08477881
Christoph Friedrich <christoph@friedrich-whv.de>
2025-03-20 12:00:00 +0100

    Implement interactive line and date

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
+
+import re   # regex for input sanitation
 
 import configparser
 config = configparser.ConfigParser()
@@ -16,12 +18,34 @@ mysql = MySQL(app)
 def frontend():
     return app.send_static_file('index.html')
 
-@app.route('/api/delays', methods=['GET'])
-def get_data():
+@app.route('/api/lines', methods=['GET'])
+def get_lines():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT line, date, tripCode, time_format(trips.departureTimePlanned, '%H:%i') AS start, time_format(trips.arrivalTimePlanned, '%H:%i') AS end, GROUP_CONCAT(time_to_sec(timediff(arrivalTimeEstimated, stopEvents.arrivalTimePlanned)) ORDER BY stopEvents.arrivalTimePlanned ASC) AS arrivalDelays, GROUP_CONCAT(time_to_sec(timediff(departureTimeEstimated, stopEvents.departureTimePlanned)) ORDER BY stopEvents.departureTimePlanned ASC) AS departureDelays FROM stopEvents LEFT JOIN stops ON stops.globalId=stopId RIGHT OUTER JOIN trips ON stopEvents.tripId=trips.id WHERE line='wvv:10010:E:H:24d' AND date=20250129 GROUP BY trips.id ORDER BY line, date, start''')
+    cur.execute('''SELECT DISTINCT line FROM `trips` ORDER BY line''')
     data = cur.fetchall()
     cur.close()
+    data_as_array = [row[0] for row in data]
+    return jsonify(data_as_array)
+
+@app.route('/api/delays', methods=['GET'])
+def get_data():
+    # input sanitation: remove all characters that are not in the list of expected characters
+    line = re.sub('[^a-zA-Z0-9:]', '', request.args.get('line', ''))
+    date = re.sub('[^0-9]', '', request.args.get('date', ''))
+    # get data from database
+    cur = mysql.connection.cursor()
+    cur.execute(f'''SELECT line, date, tripCode,
+                          time_format(trips.departureTimePlanned, '%H:%i') AS start,
+                          time_format(trips.arrivalTimePlanned, '%H:%i') AS end,
+                          GROUP_CONCAT(time_to_sec(timediff(arrivalTimeEstimated, stopEvents.arrivalTimePlanned)) ORDER BY stopEvents.arrivalTimePlanned ASC) AS arrivalDelays,
+                          GROUP_CONCAT(time_to_sec(timediff(departureTimeEstimated, stopEvents.departureTimePlanned)) ORDER BY stopEvents.departureTimePlanned ASC) AS departureDelays
+                FROM stopEvents LEFT JOIN stops ON stops.globalId=stopId RIGHT OUTER JOIN trips ON stopEvents.tripId=trips.id
+                WHERE line='{line}' AND date={date}
+                GROUP BY trips.id
+                ORDER BY line, date, start''')
+    data = cur.fetchall()
+    cur.close()
+    # output as JSON
     column_names = ['line', 'date', 'tripCode', 'start', 'end', 'arrivalDelays', 'departureDelays']
     data_as_dict = [dict(zip(column_names, row)) for row in data]
     return jsonify(data_as_dict)
